59ef1812901b297d52b43a676840578e128fff02
vithushan19 <vithushan19@gmail.com>
2022-07-21 12:36:07 -0400

    Add search functionality

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-function start() {
-  const requestURL = "https://pokeapi.co/api/v2/pokemon/haunter";
+function displayPokemon(obj) {
+  // Name
+  const name = obj.name;
+  const pokemonNameMessage = "Pokemon Name: " + name;
+  document.getElementById("name").innerHTML = pokemonNameMessage;
 
-  // Javascript Promises
-  fetch(requestURL)
+  // Image
+  const imageUrl = obj.sprites.front_default;
+  document.getElementById("image").src = imageUrl;
+
+  // Move List
+  let move1 = obj.moves[0].move;
+  let move2 = obj.moves[1].move;
+  let move3 = obj.moves[2].move;
+  let move4 = obj.moves[3].move;
+  document.getElementById("move-list").innerHTML = `<li>
+  <a href="${move1.url}">${move1.name}</a>
+  </li> <li>${move2.name}</li> <li>${move3.name}</li> <li>${move4.name}</li>`;
+}
+
+function fetchPokemon() {
+  const requestURL = "https://pokeapi.co/api/v2/pokemon/";
+
+  const searchTerm = document.getElementById("search-box").value;
+  console.log(requestURL + searchTerm);
+  fetch(requestURL + searchTerm)
     .then((x) => x.json())
     .then((obj) => {
-      // Name
-      const name = obj.name;
-      const pokemonNameMessage = "Pokemon Name: " + name;
-      document.getElementById("name").innerHTML = pokemonNameMessage;
-
-      // Image
-      const imageUrl = obj.sprites.front_default;
-      document.getElementById("image").src = imageUrl;
-
-      // Move List
-      let move1 = obj.moves[0].move.name;
-      let move2 = obj.moves[1].move.name;
-      let move3 = obj.moves[2].move.name;
-      let move4 = obj.moves[3].move.name;
-      document.getElementById(
-        "move-list"
-      ).innerHTML = `<li>${move1}</li> <li>${move2}</li> <li>${move3}</li> <li>${move4}</li>`;
+      displayPokemon(obj);
     });
 }
